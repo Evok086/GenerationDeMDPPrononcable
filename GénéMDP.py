add8dd1fb6be233ab162cc5cc03da640b40049ca
BEAUVIVRE Evan <evan.beauvivre@0441552y.paysdelaloire.education>
2024-02-15 08:30:16 +0100

    mdp de longueur 6 , optimisation et ajout du y

--- GénéMDP.py
+++ GénéMDP.py
@@ -2,9 +2,9 @@ import random
 
 def t():
     Voyelles = ['a', 'e', 'i', 'o', 'u','y']
-    var = "ttttt"
+    var = "tttttt"
     while not(var[1] in Voyelles and var[3] in Voyelles) or var[2] in Voyelles:
         var = ""
-        for i in range(5):
+        for i in range(6):
             var = var + chr(random.randint(97, 122))
     return var
