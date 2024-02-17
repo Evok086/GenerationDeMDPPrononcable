96329356b04677be52ba7782c41a8c6529e8a38a
Benji07p <Benji07p@gmail.com>
2024-02-17 19:08:49 +0100

    Importer le fichier de mot français et définir la fonction lettre_apres(letter)
    Importer le fichier de mot français
    Définir la fonction lettre_apres(letter)

--- GénéMDP.py
+++ GénéMDP.py
@@ -1,4 +1,7 @@
 import random
+with open(liste_de_mots_francais.txt, encoding='utf8') as fichier:
+    texte = fichier.read()
+
 
 def t():
     Voyelles = ['a', 'e', 'i', 'o', 'u','y']
@@ -8,3 +11,7 @@ def t():
         for i in range(6):
             var = var + chr(random.randint(97, 122))
     return var
+
+def lettre_apres(letter):
+    #Cette fonction consiste à donner une lettre aléatoire qui a une probabilité d'arriver après la lettre entrée
+    #Sauf les espaces, les caractères spéciaux et le S après le T
