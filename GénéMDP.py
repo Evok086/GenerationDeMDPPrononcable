5f3188330fd6fd98459b56c464a9fe57c6dbebe7
Benji07p <Benji07p@gmail.com>
2024-02-18 14:42:20 +0100

    enlever la fonction t()

--- GénéMDP.py
+++ GénéMDP.py
@@ -2,15 +2,6 @@ import random
 with open("liste_de_mots_francais.txt", encoding='utf8') as fichier:
     texte = fichier.read()
 
-def t():
-    Voyelles = ['a', 'e', 'i', 'o', 'u','y']
-    var = "tttttt"
-    while not(var[1] in Voyelles and var[3] in Voyelles) or var[2] in Voyelles:
-        var = ""
-        for i in range(6):
-            var = var + chr(random.randint(97, 122))
-    return var
-
 def lettre_apres(letter):
     #Cette fonction consiste à donner une lettre aléatoire qui a une probabilité d'arriver après la lettre entrée sauf les espaces et les caractères spéciaux
     j = 0
